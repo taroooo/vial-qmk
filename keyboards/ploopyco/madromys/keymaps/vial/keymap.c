ffdece83bbd445e408abc531cb67e1ae40897483
taroooo <30306621+taroooo@users.noreply.github.com>
2026-03-22 14:36:05 +0900

    Add custom keycodes for mouse control features

--- keyboards/ploopyco/madromys/keymaps/vial/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/vial/keymap.c
@@ -16,7 +16,48 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    DS_TOG = QK_KB_0,
+    DS_MOM = QK_KB_1,
+    PRECISION = QK_KB_2,
+};
+
+static bool ds_mom_active = false;
+static bool precision_active = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case DS_TOG:
+            if (record->event.pressed) {
+                ds_mom_active = !ds_mom_active;
+            }
+            return false;
+        case DS_MOM:
+            ds_mom_active = record->event.pressed;
+            return false;
+        case PRECISION:
+            precision_active = record->event.pressed;
+            return false;
+    }
+    return true;
+}
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (ds_mom_active) {
+        mouse_report.h = mouse_report.x;
+        mouse_report.v = -mouse_report.y;
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+    }
+    if (precision_active) {
+        mouse_report.x = mouse_report.x / 4;
+        mouse_report.y = mouse_report.y / 4;
+    }
+    return mouse_report;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( KC_BTN4, KC_BTN5, DRAG_SCROLL, KC_BTN2, KC_BTN1, KC_BTN3 )
+    [0] = LAYOUT( KC_BTN4, KC_BTN5, DS_MOM, KC_BTN2, KC_BTN1, KC_BTN3 )
 };
