c5430f62e4598c2abf8853a9bc59f4f9eb39487c
taroooo <30306621+taroooo@users.noreply.github.com>
2026-03-22 15:11:05 +0900

    Refactor custom keycodes and state management

--- keyboards/ploopyco/madromys/keymaps/vial/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/vial/keymap.c
@@ -1,41 +1,23 @@
-/* Copyright 2023 Colin Lam (Ploopy Corporation)
- * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
- * Copyright 2019 Sunjun Kim
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-#include QMK_KEYBOARD_H
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    DS_TOG = QK_KB_0,
-    DS_MOM = QK_KB_1,
-    PRECISION = QK_KB_2,
+    DS_TOG = QK_KB_2,
+    DS_MOM = QK_KB_3,
+    PRECISION = QK_KB_4,
 };
 
-static bool ds_mom_active = false;
+static bool ds_active = false;
 static bool precision_active = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case DS_TOG:
             if (record->event.pressed) {
-                ds_mom_active = !ds_mom_active;
+                ds_active = !ds_active;
             }
             return false;
         case DS_MOM:
-            ds_mom_active = record->event.pressed;
+            ds_active = record->event.pressed;
             return false;
         case PRECISION:
             precision_active = record->event.pressed;
@@ -45,7 +27,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (ds_mom_active) {
+    if (ds_active) {
         mouse_report.h = mouse_report.x;
         mouse_report.v = -mouse_report.y;
         mouse_report.x = 0;
